ca4a0704018a4ae8634f4ffa63d44b0f0baa937b
FiggersRM <MitchFiggers@gmail.com>
2023-05-10 15:10:49 -0400

    Completed the get fetch for the notes and added the uuid package from npm

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+const { v4: uuidv4} = require('uuid');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    console.log('Endpoint received for api/notes');
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const notes = JSON.parse(data);
+            res.send(notes);
+        }
+    })
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -14,8 +27,6 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
-app.get('api/notes', (req, res) => res.json(notes));
 
 app.listen(PORT, () =>
   console.log(`serving all static files from public on port ${PORT}!`)
