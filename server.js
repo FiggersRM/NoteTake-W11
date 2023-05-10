26b12e36be6b1c7b262fc6d802315b220d98cbe4
FiggersRM <MitchFiggers@gmail.com>
2023-05-10 14:25:24 -0400

    Pushing for help with AskBCS to render the notes for the get fetch for api/notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
@@ -8,7 +9,13 @@ app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('api/notes', (req, res) => res.json(notes));
 
 app.listen(PORT, () =>
   console.log(`serving all static files from public on port ${PORT}!`)
